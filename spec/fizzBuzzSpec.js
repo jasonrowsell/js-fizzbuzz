26c1fb45c22e939f1e089831bd514a2f8028d1cd
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-04 15:24:39 +0100

    implemented beforeEach function

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,10 +1,15 @@
 describe("fizzBuzz", function () {
-  var fizzBuzz;
+  let fizzBuzz;
+  beforeEach(function () {
+    fizzBuzz = new FizzBuzz();
+  });
 
   describe("multiples of 3", function () {
     it("returns fizz for 3", function () {
-      fizzBuzz = new FizzBuzz();
       expect(fizzBuzz.play(3)).toBe(true);
+    });
+    it("returns fizz for 6", function () {
+      expect(fizzBuzz.play(6)).toBe(true);
     });
   });
 });
