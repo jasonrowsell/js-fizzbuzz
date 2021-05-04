348f359cde80d217c992f2db6abda6fa840add2c
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-04 15:44:00 +0100

    all tests passing, fizzbuzz complete

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -12,12 +12,31 @@ describe("fizzBuzz", function () {
       expect(fizzBuzz.play(6)).toEqual("fizz");
     });
   });
+
   describe("multiples of 5", function () {
     it("returns fizz for 5", function () {
       expect(fizzBuzz.play(5)).toEqual("buzz");
     });
     it("returns fizz for 10", function () {
       expect(fizzBuzz.play(10)).toEqual("buzz");
+    });
+  });
+
+  describe("multiples of 15", function () {
+    it("returns fizz for 15", function () {
+      expect(fizzBuzz.play(15)).toEqual("fizzbuzz");
+    });
+    it("returns fizz for 30", function () {
+      expect(fizzBuzz.play(30)).toEqual("fizzbuzz");
+    });
+  });
+
+  describe("not a multiple of 3 or 5", function () {
+    it("returns the integer", function () {
+      expect(fizzBuzz.play(7)).toEqual(7);
+    });
+    it("returns fizz for 30", function () {
+      expect(fizzBuzz.play(19)).toEqual(19);
     });
   });
 });
