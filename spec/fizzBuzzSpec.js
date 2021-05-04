7e8f1942c90e4aab0f7590964d4aeffdbafe3a70
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-04 15:50:14 +0100

    refactored to arrow functions

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,41 +1,41 @@
-describe("fizzBuzz", function () {
+describe("fizzBuzz", () => {
   let fizzBuzz;
   beforeEach(function () {
     fizzBuzz = new FizzBuzz();
   });
 
-  describe("multiples of 3", function () {
-    it("returns fizz for 3", function () {
+  describe("multiples of 3", () => {
+    it("returns fizz for 3", () => {
       expect(fizzBuzz.play(3)).toEqual("fizz");
     });
-    it("returns fizz for 6", function () {
+    it("returns fizz for 6", () => {
       expect(fizzBuzz.play(6)).toEqual("fizz");
     });
   });
 
-  describe("multiples of 5", function () {
-    it("returns fizz for 5", function () {
+  describe("multiples of 5", () => {
+    it("returns fizz for 5", () => {
       expect(fizzBuzz.play(5)).toEqual("buzz");
     });
-    it("returns fizz for 10", function () {
+    it("returns fizz for 10", () => {
       expect(fizzBuzz.play(10)).toEqual("buzz");
     });
   });
 
-  describe("multiples of 15", function () {
-    it("returns fizz for 15", function () {
+  describe("multiples of 15", () => {
+    it("returns fizz for 15", () => {
       expect(fizzBuzz.play(15)).toEqual("fizzbuzz");
     });
-    it("returns fizz for 30", function () {
+    it("returns fizz for 30", () => {
       expect(fizzBuzz.play(30)).toEqual("fizzbuzz");
     });
   });
 
-  describe("not a multiple of 3 or 5", function () {
-    it("returns the integer", function () {
+  describe("not a multiple of 3 or 5", () => {
+    it("returns the integer", () => {
       expect(fizzBuzz.play(7)).toEqual(7);
     });
-    it("returns fizz for 30", function () {
+    it("returns fizz for 30", () => {
       expect(fizzBuzz.play(19)).toEqual(19);
     });
   });
